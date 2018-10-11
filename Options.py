96916c42a15442c05ec8c880d8bf2146a159fd09
LucasDenele <lucas.denele@isen-lille.fr>
2018-10-11 17:32:05 +0200

    Loggers Creation

--- Options.py
+++ Options.py
@@ -5,35 +5,65 @@
 
 # imports:
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 # constants:
+LOGGER = 0
+
 
 # ----------------------------
 # Part 2 - all functions
 #
 # ----------------------------
+def create_loggers(file_localisation_all: str, file_localisation_error: str):
+    """ Create the logger for all information"""
+
+    # Create the logging objects
+    logger = logging.getLogger()
+
+    # Create the logs format
+    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+
+    # Create the handler, with a security space. The log file will have 1Mo max size
+    file_handler = RotatingFileHandler(file_localisation_all, 'a', 1000000, 1)
+
+    # Apply the handler to the logger
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # Do handler for the error logger
+    file_handler = RotatingFileHandler(file_localisation_error, 'a', 1000000, 1)
+    file_handler.setLevel(logging.ERROR)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
 def json_parse(file: dict):
     """ Parse the json file to get the informations for the connections"""
 
     # ==== Errors handler ====
     # Check if the access_token has given
     if None == file.get('access_token'):
-        raise NameError("KeyError : 'access_token' has not been given")
+        LOGGER.error("KeyError : 'access_token' has not been given")
     # Check if the access_token has given
     if None == file.get("protocole"):
-        raise NameError("KeyError : 'protocole' has not been given")
+        LOGGER.error("KeyError : 'protocole' has not been given")
     # Check if the access_token has given
     if None == file.get("users"):
-        raise NameError("KeyError : 'users' has not been given")
+        LOGGER.error("KeyError : 'users' has not been given")
     # Check if the access_token has given
     if None == file.get("passwords"):
-        raise NameError("KeyError : 'passwords' has not been given")
+        LOGGER.error("KeyError : 'passwords' has not been given")
     # Check if there are the same numbers of user names and passwords
     if len(file["users"]) > len(file["passwords"]):
-        raise NameError("FormatError : missing " + str(len(file["users"]) - len(file["passwords"])) + " password(s)")
+        LOGGER.error("FormatError : missing " + str(len(file["users"]) - len(file["passwords"])) + " password(s)")
     if len(file["passwords"]) > len(file["users"]):
-        raise NameError("FormatError : missing " + str(len(file["passwords"]) - len(file["users"])) + " user name(s)")
+        LOGGER.error("FormatError : missing " + str(len(file["passwords"]) - len(file["users"])) + " user name(s)")
 
     # ==== Return ===
     # return the different parts of the needed informations
@@ -59,3 +89,5 @@ def get_connections_informations(json_localisation: str):
 if __name__ == "__main__":
     """
     """
+    LOGGER = create_loggers("D:/ISEN/Python/Checkeur/CheckeurProject/logAll.txt", "D:/ISEN/Python/Checkeur/CheckeurProject/logError.txt")
+    get_connections_informations("D:/ISEN/Python/Checkeur/CheckeurProject/checkeur.json")
